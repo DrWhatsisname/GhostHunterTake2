295a864f8dd7961457bf01e45fb6d59e8cf0848e
Henry Conkin <hwc5gj@virginia.edu>
2014-11-16 14:42:59 -0500

    Made a change

--- GhostHunter17/src/edu/virginia/cs2110/ghosthunter17/SplashScreen.java
+++ GhostHunter17/src/edu/virginia/cs2110/ghosthunter17/SplashScreen.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
