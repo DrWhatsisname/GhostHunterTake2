30131e65ee5fb6d5f37606826bd32475641d8627
Henry Conkin <hwc5gj@virginia.edu>
2014-11-23 18:34:42 -0500

    Revert "Revert "Added movement helper functions to GameObject""
    This reverts commit 49a69b67d6ce852e8e990b01ddb4716b38bdfe73.

--- GhostHunter17/src/edu/virginia/cs2110/ghosthunter17/GameObject.java
+++ GhostHunter17/src/edu/virginia/cs2110/ghosthunter17/GameObject.java
@@ -8,7 +8,7 @@ public abstract class GameObject {
 
 	protected PointF pos;
 	protected World world;
-	
+
 	public GameObject(World world) {
 		this.pos = new PointF();
 		this.world = world;
@@ -23,12 +23,55 @@ public abstract class GameObject {
 		this.world = world;
 		this.pos = pos;
 	}
-	
+
 	public abstract RectF getColBounds();
-	
+
 	public abstract void update(float timePassed);
 
 	public abstract void render(Canvas c);
-	
+
 	public abstract void collide(GameObject g);
+
+	protected void moveTo(PointF target, float timePassed, float velocity) {
+		PointF dist = new PointF(target.x - pos.x, target.y - pos.y);
+		
+		//Move towards target
+		float len = dist.length();
+		float scale = velocity / len * timePassed;
+		PointF diff = new PointF(dist.x * scale, dist.y * scale);
+		// Move towards the target if it is further than the player can
+		// travel in a frame
+		if (diff.length() < len) {
+			pos.x += diff.x;
+			pos.y += diff.y;
+		}
+		// Otherwise move to the target
+		else {
+			pos.x += dist.x;
+			pos.y += dist.y;
+		}
+	}
+	
+	protected float rotateTo(float angleTo, float angleFrom, float timePassed, float velocity) {
+		//Rotate towards target
+		float frameRot = angleTo - angleFrom;
+		if (Math.abs(angleTo + 360 - angleFrom) < Math.abs(frameRot)) {
+			frameRot = angleTo + 360 - angleFrom;
+		} else if (Math.abs(angleTo - 360 - angleFrom) < Math.abs(frameRot)) {
+			frameRot = angleTo - 360 - angleFrom;
+		}
+		
+		if (frameRot!=0){
+			float normalFrameRot = frameRot / Math.abs(frameRot);
+			normalFrameRot *= velocity * timePassed;
+
+			if (Math.abs(frameRot) < Math.abs(normalFrameRot)) {
+				angleFrom += frameRot;
+			} else {
+				angleFrom += normalFrameRot;
+			}
+			angleFrom%=360;
+		}
+		return angleFrom;
+	}
 }
